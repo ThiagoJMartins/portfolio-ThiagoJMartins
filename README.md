90bd9a559ac4a2c60e9d675eb42d8bc70cb6dd57
ThiagoJMartins <thiagodev.martins@gmail.com>
2023-12-31 13:50:52 -0300

    Definitive Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Personal Portfolio
+
+This is Thiago Martins's personal porfolio.
+
+This website was created with JavaScript, Astro and TailwindCSS.
+
+I hope u enjoy it
+
+## Demo
+
+https://thiagomartins.vercel.app/
---
+++ src/utils/projects-resource.js
@@ -0,0 +1,48 @@
+export const data = [
+	{
+		name: "NotesAres",
+		image: "/projects/notesares.png",
+		description: "Notes app",
+		demo: "https://notesares.netlify.app/",
+	},
+	{
+		name: "Personal Porfolio",
+		image: "/projects/portfolio.png",
+		description: "Personal portfolio with Astro and Tailwind",
+		demo: "https://thiagomartins.vercel.app",
+	},
+	{
+		name: "Rick & Morty",
+		image: "/projects/rick&morty.png",
+		description: "Rick and Morty API Cards Website",
+		demo: "",
+	},
+];
+
+export const links = [
+	{
+		link: "home",
+		name: "Home",
+		icon: "home.svg",
+	},
+	{
+		link: "about",
+		name: "About Me",
+		icon: "about.svg",
+	},
+	{
+		link: "experience",
+		name: "Experience",
+		icon: "experience.svg",
+	},
+	{
+		link: "my-job",
+		name: "My Work",
+		icon: "my-job.svg",
+	},
+	{
+		link: "contact",
+		name: "Contact Me",
+		icon: "contact.svg",
+	},
+];

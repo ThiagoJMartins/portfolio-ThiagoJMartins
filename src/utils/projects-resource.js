636f3d45933dff2a0825e51202b8c942ec6699a5
Ares <thiagodev.martins@gmail.com>
2024-03-17 15:13:40 -0300

    Add new experience section and update projects and links

--- src/utils/projects-resource.js
+++ src/utils/projects-resource.js
@@ -1,49 +1,55 @@
 export const data = [
-  {
-    name: "NotesAres",
-    image: "/projects/notesares.webp",
-    description: "Notes app",
-    demo: "https://notesares.netlify.app/",
-  },
-  {
-    name: "Personal Porfolio",
-    image: "/projects/portfolio.webp",
-    description: "Personal portfolio with Astro and Tailwind",
-    demo: "https://thiagomartins.vercel.app",
-  },
-  {
-    name: "GreenPortal",
-    image: "/projects/rick&morty.webp",
-    description: "Rick and Morty API Cards Website",
-    demo: "https://greenportal.vercel.app",
-  },
-  {
-    name: "Ares Tac Toe",
-    image: "/projects/arestactoe.webp",
-    description: "Tic Tac Toe game created with React",
-    demo: "https://arestactoe.vercel.app",
-  },
+	{
+		name: "Ares GamesDB",
+		image: "/projects/aresgamesdb.webp",
+		description: "Games Database with RAWG API",
+		demo: "https://ares-gamesdb.vercel.app",
+	},
+	{
+		name: "Personal Porfolio",
+		image: "/projects/portfolio.webp",
+		description: "Personal portfolio with Astro and Tailwind",
+		demo: "https://thiagomartins.vercel.app",
+	},
+	{
+		name: "GreenPortal",
+		image: "/projects/rick&morty.webp",
+		description: "Rick and Morty API Cards Website",
+		demo: "https://greenportal.vercel.app",
+	},
+	{
+		name: "Ares Tac Toe",
+		image: "/projects/arestactoe.webp",
+		description: "Tic Tac Toe game created with React",
+		demo: "https://arestactoe.vercel.app",
+	},
+	{
+		name: "NotesAres",
+		image: "/projects/notesares.webp",
+		description: "Notes app",
+		demo: "https://notesares.netlify.app/",
+	},
 ];
 
 export const links = [
-  {
-    link: "home",
-    name: "Home",
-    icon: "home.svg",
-  },
-  {
-    link: "about",
-    name: "About Me",
-    icon: "about.svg",
-  },
-  {
-    link: "experience",
-    name: "Experience",
-    icon: "experience.svg",
-  },
-  {
-    link: "my-job",
-    name: "My Work",
-    icon: "my-job.svg",
-  },
+	{
+		link: "home",
+		name: "Home",
+		icon: "home.svg",
+	},
+	{
+		link: "about",
+		name: "About Me",
+		icon: "about.svg",
+	},
+	{
+		link: "experience",
+		name: "Experience",
+		icon: "experience.svg",
+	},
+	{
+		link: "my-job",
+		name: "My Work",
+		icon: "my-job.svg",
+	},
 ];
